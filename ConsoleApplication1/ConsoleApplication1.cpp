d9da3f4d058aaa7b6f44cc87d7a0b37d298a164d
Kostr_ev79 <Kostr_ev79@8-11>
2024-11-08 18:36:14 +0200

    Add surname field

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,36 +5,48 @@ using namespace std;
 
 class Student {
 	string name;
+	string surname;
 	int age;
+	
 public:
 	Student() {
 		name = "Kostya";
+		surname = "Kostrov";
 		age = 222;
 	}
-		Student(string name, int age) {
+		Student(string name, string surname, int age) {
 			this->name = name;
+			this->surname = surname;
 			this->age = age;
 		}
 
 		Student(const Student& obj) {
 			this->name = obj.name;
+			this->surname = obj.surname;
 			this->age = obj.age;
 
 		}
 
 		void PrintInfoAboutStudent() {
 			cout << "Student`s name: " << this->name << endl;
+			cout << "Student`s surname: " << this->surname << endl;
 			cout << "Student`s age: " << this->age << endl;
 		}
 		void ChangeInfoAboutStudent() {
 			cout << "Enter a new name for student: ";
 			cin >> this->name;
+			cout << "Enter a new surname for student: ";
+			cin >> this->surname;
 			cout << "Enter a new age for student: ";
 			cin >> this->age;
 	}
 
 		string getName() {
 			return this->name;
+		}
+
+		string getSurname() {
+			return this->surname;
 		}
 
 		int getAge() {
@@ -44,6 +56,10 @@ public:
 
 		void setName(const string name) {
 			this->name = name;
+		}
+
+		void setSurname(const string surname) {
+			this->surname = surname;
 		}
 
 		void setAge(const int age) {
